ad57f6d83fedaecb797f195ae16d5f8e0b3b8dc4
Besim Mustafi <besimnaim@gmail.com>
2024-02-11 23:45:56 +0100

    Add loadCard function and findeBackgroundColor function

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+let pokemonTypes = [
+  "water",
+  "electric",
+  "fire",
+  "grass",
+  "poison",
+  "bug",
+  "flying",
+  "normal",
+  "ground",
+  "fairy",
+  "fighting",
+  "psychic",
+  "rock",
+  "ice",
+  "ghost",
+  "dragon",
+];
+
+let backgroundColors = [
+  "rgb(104,144,240,0.7)",
+  "rgb(248,208,48,0.7)",
+  "rgb(240,128,48,0.7)",
+  "rgb(120,200,80,0.7)",
+  "rgb(160,64,160,0.7)",
+  "rgb(168,184,32,0.7)",
+  "rgb(168,144,240,0.7)",
+  "rgb(168,168,120,0.7)",
+  "rgb(224,192,104,0.7)",
+  "rgb(238,153,172,0.7)",
+  "rgb(192,48,40,0.7)",
+  "rgb(248,88,136,0.7)",
+  "rgb(184,160,56,0.7)",
+  "rgb(152,216,216,0.7)",
+  "rgb(112,88,152,0.7)",
+  "rgb(112,56,248,0.7)",
+];
+
 let currentPokemon;
 
 async function loadPokemon() {
@@ -5,4 +43,23 @@ async function loadPokemon() {
   let response = await fetch(url);
   currentPokemon = await response.json();
   console.log(currentPokemon);
+  loadCard();
+}
+
+function loadCard() {
+  findeBackgroundColor();
+  let pokeCard = document.getElementById("card");
+  pokeCard.innerHTML += `
+  <div style="background-color: ${findeBackgroundColor()}" id="container"> <img id="imagePokemon" alt=""></div>
+  <div id="informationPokemon"></div>`;
+}
+
+function findeBackgroundColor() {
+  let pokeType = currentPokemon["types"]["0"]["type"]["name"];
+  let backgroundColor;
+  if (pokemonTypes.includes(pokeType)) {
+    let indexType = pokemonTypes.indexOf(pokeType);
+    backgroundColor = backgroundColors[indexType];
+  }
+  return backgroundColor;
 }
